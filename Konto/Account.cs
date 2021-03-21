4864400a7bca9c7ce383c02afc76b9f232d08577
Joanna <j.prajzendanc@gmail.com>
2021-03-21 12:57:03 +0100

    init

---
+++ Konto/Account.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Konto
+{
+    class Account
+    {
+        private String user;
+        private String number;
+        private String login;
+        private String password;
+        private bool logged; //na wstępie zawsze false
+        private double saldo = 0;
+
+        public Account(String user, String num, String log, String pass)
+        {
+            this.user = user; number = num; password = pass;
+            login = log;
+        }
+
+        public void Zaloguj(String log, String pass)
+        {
+            if ((login == log) && (password == pass)) { logged = true; }
+        }
+
+        public override String ToString()
+        {
+            return "\nUżytkownik konta " + user + " o numerze " + number +
+                   " posiada na koncie " + saldo;
+        }
+    }
+}
---
+++ Konto/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Konto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account k = new Account("Jan Kowalski", "37562", "mała", "czarna");
+            k.Zaloguj("mała", "czarna");
+            Console.WriteLine(k);
+            Console.WriteLine("Naciśnij dowolny klawisz");
+            Console.ReadKey();
+        }
+    }
+}
